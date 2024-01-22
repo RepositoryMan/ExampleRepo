588f23f8a791adafa6283d4189d7a91a41eb5f02
RepositoryMan <102048146+RepositoryMan@users.noreply.github.com>
2024-01-22 08:47:15 -0800

    Update ExamplePython.py
    deleted password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,12 +1,6 @@
-def test_password(x):
-    print(x)
-
 print("Im not writing all that")
 
 job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
     print(f"{name} is a {job}")
-
-password = "SuperSecretAdminPassword"
-test_password(password)
